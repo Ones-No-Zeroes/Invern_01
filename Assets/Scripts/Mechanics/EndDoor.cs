f70001e98146097631ac34e2c40299d757b258b1
L-Dugue <ldugue2007@outlook.com>
2026-02-23 17:11:46 -0500

    [ADD] Added Chris' level

---
+++ Assets/Scripts/Mechanics/EndDoor.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndDoor : MonoBehaviour
+{
+    public bool finalLevel;
+    public string nextLevelName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            if(finalLevel == true)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                SceneManager.LoadScene(nextLevelName);
+            }
+
+        }
+    }
+
+}
