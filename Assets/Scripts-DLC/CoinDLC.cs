2f423c0fde9fb6bf8b7d0571541af5252347022c
Chris Trombly <christrombly001@hotmail.com>
2026-02-27 15:09:13 -0500

    Checkpoint #2
    +Enemies
    +Player
    - Tilesets aren't using correct settings, character falls through the outside permiter of tiles.

---
+++ Assets/Scripts-DLC/CoinDLC.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CoinDLC : MonoBehaviour
+{
+    public int scoreToGive;
+    public float bobHeight;
+    public float bobSpeed;
+
+    private float startYPos;
+
+    private void Start()
+    {
+        startYPos = transform.position.y;
+    }
+
+    private void Update()
+    {
+        float newY = startYPos + (Mathf.Sin(Time.time * bobSpeed) * bobHeight);
+        transform.position = new Vector3(transform.position.x, newY, 0);
+    }
+
+
+    //My Code
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<Player>().AddScore(scoreToGive);
+            Destroy(this.gameObject);
+        }
+    }
+}
