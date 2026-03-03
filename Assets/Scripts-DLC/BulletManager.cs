28171bc2561c38529bcfe056b8de9c71934f6e57
Chris Trombly <christrombly001@hotmail.com>
2026-03-02 22:49:50 -0500

    Magic Bullet V1

---
+++ Assets/Scripts-DLC/BulletManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+
+    public GameObject bullet;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("MagicBullet", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            MagicBullet();
+        }
+    }
+
+    void MagicBullet()
+    {
+        Instantiate(bullet, new Vector3(45f, 28.465f, 0), bullet.transform.rotation);
+    }
+}
