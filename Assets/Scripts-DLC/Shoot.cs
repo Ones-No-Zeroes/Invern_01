c37fd72603325f7cb8b4451649908955ba4f0fd6
Chris Trombly <christrombly001@hotmail.com>
2026-03-02 22:30:24 -0500

    Bullet Launch & Gravity
    Only on start so far

--- Assets/Scripts-DLC/Shoot.cs
+++ Assets/Scripts-DLC/Shoot.cs
@@ -2,18 +2,26 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-
+    public Rigidbody2D rb;
     public float speed = 20f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(Vector2.right * speed, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.right * Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.gravityScale = -2;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rb.gravityScale = 2;
+        }
     }
 }
