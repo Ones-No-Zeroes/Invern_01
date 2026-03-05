8d2b76a3f68470394c4e07c880cb582bdc39eb98
Chris Trombly <christrombly001@hotmail.com>
2026-03-05 17:13:04 -0500

    Added HP logic to Magic Bullet Target
    No UI for boss fight section 1

--- Assets/Scripts-DLC/DetectCollision.cs
+++ Assets/Scripts-DLC/DetectCollision.cs
@@ -2,9 +2,23 @@ using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject);
-        Destroy(other.gameObject);
-    }
+        //other.GetComponent<LockBoss>().LoseHP();
+        //Destroy(other.GetComponent<Shoot>());
+
+
+        if (collision.CompareTag("Bullet"))
+        {
+            Destroy(collision.gameObject);
+        }
+        else if (collision.CompareTag("Target"))
+        {
+            Debug.Log("Hit");
+            collision.GetComponent<LockBoss>().LoseHP();
+        }
+
+            //Destroy(this.gameObject);
+            //Destroy(other.gameObject);
+        }
 }
---
+++ Assets/Scripts-DLC/LockBoss.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LockBoss : MonoBehaviour
+{
+    public int healthPoints = 10;
+    public float moveSpeed;
+    public Vector3 moveOffset;
+    private Vector3 startPos;
+    private Vector3 targetPos;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPos = transform.position;
+        targetPos = startPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+
+        if (transform.position == targetPos)
+        {
+
+            if (targetPos == startPos)
+            {
+                targetPos = startPos + moveOffset;
+            }
+            else
+            {
+                targetPos = startPos;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 from;
+        Vector3 to;
+        if (Application.isPlaying)
+        {
+            from = startPos;
+        }
+        else
+        {
+            from = transform.position;
+        }
+
+        to = from + moveOffset;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(from, to);
+        Gizmos.DrawWireSphere(to, 0.2f);
+        Gizmos.DrawWireSphere(from, 0.2f);
+    }
+    public void LoseHP()
+    {
+        healthPoints -= 1;
+        if (healthPoints < 0)
+        {
+
+            BossDefeat();
+        }
+
+    }
+
+    private void BossDefeat()
+    {
+        Debug.Log("Winner!");
+        Destroy(this.gameObject);
+        SceneManager.LoadScene(0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //other.GetComponent<LockBoss>().LoseHP();
+        //Destroy(other.GetComponent<Shoot>());
+    }
+}
