31b67a61cb1fd33bf3b871bf97c1eb41353a33b4
L-Dugue <ldugue2007@outlook.com>
2026-03-14 10:35:36 -0400

    [MOD] Improved Boss movement

--- Assets/Scripts/Enemy/Boss/BossMovement.cs
+++ Assets/Scripts/Enemy/Boss/BossMovement.cs
@@ -1,7 +1,89 @@
 using System;
 using UnityEngine;
 
-public class BossMovement : BasicEnemy
+public class BossMovement : MonoBehaviour
 {
+    // Serialized Fields 
+    [Header("Boss Movement Attributes")]
+    [SerializeField] private float offSetFromStart;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Rigidbody2D rigidBody;
+
+    // Private Fields
+    private float startPositionX;
+    private float endPositionX;
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        startPositionX = transform.position.x;
+        endPositionX = startPositionX + offSetFromStart;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Patrol();
+    }
+
+    
+
+    private void Patrol()
+    {   
+        if(transform.position.x < startPositionX)
+        {
+            endPositionX = startPositionX + offSetFromStart;
+        }
+        else if(transform.position.x > startPositionX + offSetFromStart)
+        {
+            endPositionX = startPositionX;
+        }
+        
+
+        if((int)transform.position.x != (int)endPositionX)
+        {
+            if(endPositionX == startPositionX)
+            {
+                rigidBody.linearVelocity = new Vector2((-1 * moveSpeed * 2) * Time.deltaTime, rigidBody.linearVelocity.y); // For some reason it freezes the character if the moveSpeed is above 60?
+            }
+            else
+            {
+                rigidBody.linearVelocity = new Vector2((moveSpeed * 2) * Time.deltaTime, rigidBody.linearVelocity.y);
+            }
+        }
+        else
+        {
+            if(endPositionX == startPositionX)
+            {
+                endPositionX = startPositionX + offSetFromStart;
+            }
+            else
+            {
+                endPositionX = startPositionX;
+            }
+            
+        }
+    }
+     private void OnDrawGizmos()
+    {
+        Vector3 from;
+        Vector3 to;
+        if (Application.isPlaying)
+        {
+            from = new Vector3(startPositionX, transform.position.y, 0);
+        }
+        else
+        {
+            from = transform.position;
+        }
+
+        to = new Vector3(from.x + offSetFromStart, transform.position.y, 0);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(from, to);
+        Gizmos.DrawWireSphere(to, 0.2f);
+        Gizmos.DrawWireSphere(from, 0.2f);
+    }
     
 }
