6716cc240a0c6013e4e5add76904eb749fbdd811
Chris Trombly <christrombly001@hotmail.com>
2026-03-25 22:20:06 -0400

    Slime Getaway Recovered

---
+++ Assets/Scripts-DLC/SolidZone.cs
@@ -0,0 +1,70 @@
+
+using UnityEngine;
+
+public class SolidZone : MonoBehaviour
+{
+
+    [SerializeField] private Color debugColor = Color.white;
+
+    public float moveSpeed;
+    public Vector3 moveOffset;
+    private Vector3 startPos;
+    private Vector3 targetPos;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPos = transform.position;
+        targetPos = startPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+
+        if (transform.position == targetPos)
+        {
+
+            if (targetPos == startPos)
+            {
+                targetPos = startPos + moveOffset;
+            }
+            else
+            {
+                targetPos = startPos;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = debugColor;
+        BoxCollider2D box = GetComponent<BoxCollider2D>();
+
+        if (box != null)
+        {
+            Gizmos.DrawCube(transform.position + (Vector3)box.offset, box.size);
+        }
+
+        Vector3 from;
+        Vector3 to;
+        if (Application.isPlaying)
+        {
+            from = startPos;
+        }
+        else
+        {
+            from = transform.position;
+        }
+
+        to = from + moveOffset;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(from, to);
+        Gizmos.DrawWireSphere(to, 0.2f);
+        Gizmos.DrawWireSphere(from, 0.2f);
+    }
+
+}
